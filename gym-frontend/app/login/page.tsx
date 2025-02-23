d2b3be1af5153d3acd68b2f3ed00433d22b4f4e4
coconal <m15196712167@163.com>
2025-02-23 22:13:38 +0800

    feat:login

--- gym-frontend/app/login/page.tsx
+++ gym-frontend/app/login/page.tsx
@@ -1,48 +1,141 @@
 "use client"
 
+import { Alert, AlertProps, Button, Snackbar, SnackbarOrigin } from "@mui/material"
+import { Flex } from "antd"
+import { useState } from "react"
 import { useAccount, useSignMessage } from "wagmi"
+import LoginIcon from "@mui/icons-material/Login"
+
+interface SnackbarState extends SnackbarOrigin {
+	open: boolean
+}
 
 export default function LoginPage() {
+	const [state, setState] = useState<SnackbarState>({
+		open: true,
+		vertical: "top",
+		horizontal: "center",
+	})
+	const [error, setError] = useState<{
+		open: boolean
+		message: string
+	}>({
+		open: false,
+		message: "",
+	})
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const { open } = state
 	const { address, isConnected } = useAccount()
-	const { signMessageAsync, isPending } = useSignMessage()
+	const { signMessageAsync } = useSignMessage()
+
+	const handleClose = () => {
+		setState({ ...state, open: false })
+	}
 
 	const handleLogin = async () => {
 		try {
-			if (!isConnected) return alert("请先连接钱包")
-
 			// 生成防重放消息
-			const nonce = Math.random().toString(36).substring(2)
-			const message = `Web3登录验证:\nNonce: ${nonce}`
-
-			// 获取签名
-			const signature = await signMessageAsync({ message })
-
-			// 发送到后端验证
-			const res = await fetch("/api/auth/login", {
-				method: "POST",
+			setIsLoading(true)
+			// // 发送到后端验证
+			const res = await fetch(`http://localhost:3002/api/user/nonce/${address}`, {
+				method: "GET",
 				headers: { "Content-Type": "application/json", Authorization: `Bearer 123` },
-				body: JSON.stringify({ address, message, signature }),
 			})
 
 			const data = await res.json()
-
+			// console.log(data)
+			const message = `login message&nunce:${data.nonce}`
+			const signature = await signMessageAsync({ message })
+			const res2 = await fetch(`http://localhost:3002/api/user/login`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					address: address,
+					signature: signature,
+				}),
+			})
+			const data2 = await res2.json()
+			// console.log(data2)
 			if (data.token) {
-				localStorage.setItem("web3-token", data.token)
+				localStorage.setItem("web3-token", data2.token)
 			}
-		} catch (error) {
-			console.error("登录失败:", error)
+		} catch (err: any) {
+			setError({
+				open: true,
+				message: `Something went wrong: ${err.message}`,
+			})
 		} finally {
+			setIsLoading(false)
 		}
 	}
 
 	return (
 		<div>
-			<h1>Web3 登录示例</h1>
-			<button onClick={handleLogin} disabled={isPending}>
-				{isPending ? "处理中..." : "签名登录"}
-			</button>
-			<h1>Web3 写入示例</h1>
-			<button onClick={handleLogin}>{isPending ? "处理中..." : "签名登录"}</button>
+			{isConnected ? (
+				<>
+					<Flex justify="center" align="center" style={{ height: "80vh" }}>
+						<Button
+							loading={isLoading}
+							loadingPosition="start"
+							startIcon={<LoginIcon />}
+							sx={{
+								backgroundColor: "rgba(39, 137, 228, 0.919)",
+								padding: "12px 26px",
+								fontSize: "1.2rem",
+								color: "#ffffff",
+								"& .MuiSvgIcon-root": {
+									color: "#ebeff3",
+									fontSize: "1.5rem",
+								},
+								"&:hover": {
+									backgroundColor: "rgba(199, 212, 224, 0.04)",
+								},
+							}}
+							onClick={handleLogin}
+						>
+							Login with wallet signature
+						</Button>
+					</Flex>
+					<Notifaction
+						SnackbarOpen={error.open}
+						handleClose={() => setError({ ...error, open: false })}
+						severity="error"
+						variant="filled"
+						content={error.message}
+					/>
+				</>
+			) : (
+				<Notifaction
+					SnackbarOpen={open}
+					handleClose={handleClose}
+					severity="info"
+					variant="filled"
+					content="Please Connect Wallet"
+				/>
+			)}
 		</div>
+	)
+}
+
+interface NotifactionProps {
+	SnackbarOpen: boolean
+	handleClose: () => void
+	severity: AlertProps["severity"]
+	variant: AlertProps["variant"]
+	content: string
+}
+
+function Notifaction({ ...props }: NotifactionProps) {
+	return (
+		<Snackbar open={props.SnackbarOpen} autoHideDuration={6000}>
+			<Alert
+				onClose={props.handleClose}
+				severity={props.severity}
+				variant={props.variant}
+				sx={{ width: "100%" }}
+			>
+				{props.content}
+			</Alert>
+		</Snackbar>
 	)
 }
